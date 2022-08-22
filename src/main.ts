710fe4b75a409e9e62d17d8d1d81f3dafcad59e6
dedenurr <dedenurrahman.git@gmail.com>
2022-08-22 13:44:35 +0700

    user module

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //dto akan memfilter field yang tidak sesuai dan tidak terdaftar di dto tidak akan masuk ke dalam kontroller
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
